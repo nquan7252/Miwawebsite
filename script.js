2b40792b00b5d63b9394dd200f57ada5ad9f1367
nquan7252 <81738938+nquan7252@users.noreply.github.com>
2023-09-15 12:44:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,8 @@ document.addEventListener("scroll", () => {
     sidebar.style.pointerEvents = "auto";
     sidebar.style.filter = "invert(0)";
     sidebar.style.opacity = "1";
-    icons.querySelector("#homeicon")?.classList.add("active")
+    icons.forEach(icon=>icon.classList.remove("active"))
+    document.querySelector("#abouticon")?.classList.add("active")
   } else {
     sidebar.style.opacity = "0";
     sidebar.style.pointerEvents = "none";
